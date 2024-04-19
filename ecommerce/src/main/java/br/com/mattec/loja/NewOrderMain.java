779a7ead57d21fa3d929b84c36dfbac1b0302f1f
jessemusic <jmc_cursos@hotmail.com>
2024-04-19 15:45:14 -0300

    Ajuste do gitignore

--- ecommerce/src/main/java/br/com/mattec/loja/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/mattec/loja/NewOrderMain.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
     try(var dispatcher = new KafkaDispatcher()){
