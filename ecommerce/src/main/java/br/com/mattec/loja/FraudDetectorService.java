f8eb407b9641c54d3aad5e9fd8f7ffec046bd8f5
jessemusic <jmc_cursos@hotmail.com>
2024-04-20 08:55:27 -0300

    Customizando LogService e criando um biblioteca propria para deserializar any dados

--- ecommerce/src/main/java/br/com/mattec/loja/FraudDetectorService.java
+++ ecommerce/src/main/java/br/com/mattec/loja/FraudDetectorService.java
@@ -3,15 +3,20 @@ package br.com.mattec.loja;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
-                fraudService::parse);
+        var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of());
         service.run();
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("---------------------------------------------");
         System.out.println("Processando0 new order, checking for fraud!");
         System.out.println(record.key());
--- ecommerce/src/main/java/br/com/mattec/loja/LogService.java
+++ ecommerce/src/main/java/br/com/mattec/loja/LogService.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
@@ -13,7 +14,9 @@ public class LogService {
         var logService = new LogService();
         try (var service = new KafkaService(LogService.class.getSimpleName(),
                 Pattern.compile("ECOMMERCE.*"),
-                logService::parse)) {
+                logService::parse,
+                String.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
             service.run();
         }
     }
